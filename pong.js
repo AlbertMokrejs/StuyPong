6e6ce8f9e39442c0af2b0efc8324c4864ff5c229
Chris Grant <chris@chrisgrant.co>
2016-03-12 17:07:54 -0500

    updates

--- pong.js
+++ pong.js
@@ -1,25 +1,19 @@
 var canvas = document.getElementById("game");
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 68 - 19 - 40;
+
 var ctx = canvas.getContext("2d");
-
-var clr = document.getElementById("clear");
-var eas = document.getElementById("easy");
-var med = document.getElementById("med");
-var hrd = document.getElementById("hard");
-
-var powerups = [];
-var paddlev = 0;
-var plength = canvas.height / 6;
-var pheight = 10;
-var pycor = canvas.height / 2 - (plength / 2);
-
-var pad = new Image();
-pad.src = "paddle.png";
-
-var ball = new Image();
-ball.src = "ball.png";
 
 ctx.fillStyle = "#f0f000";
 ctx.strokeStyle = "#00f0f0";
+
+var Powerups = [];
+
+var PaddleMeta = {
+  length: canvas.height / 6,
+  height: 10,
+  img: new Image()
+}
 
 var Ball = {
   coors: {
@@ -30,31 +24,116 @@ var Ball = {
     y: 0,
     x: 0
   },
-  move: function() {
+  img: new Image(),
+  move: function(x, y) {
+    if (Ball.coors.x > canvas.width - 50 && Ball.coors.y > Computer.coors.y && Ball.coors.y < Computer.coors.y + 30) {
+      Ball.velocity.x *= -1
+    } else if (Ball.coors.x < 40 && Ball.coors.y > Player.coors.y && Ball.coors.y < Player.coors.y + PaddleMeta.length) {
+      Ball.velocity.x += Player.velocity / 2
+      Ball.velocity.x *= -1
+    } else if (Ball.coors.x < 40 && Ball.coors.y > Computer.coors.y && Ball.coors.y < Computer.coors.y + PaddleMeta.length) {
+      Ball.velocity.x += Computer.velocity / 2
+      Ball.velocity.x *= -1
+    }
 
+    if (Ball.coors.y > canvas.height || Ball.coors.y < 0) {
+      Ball.velocity.y *= -1
+    }
+
+    Ball.coors.x += Ball.velocity.x;
+    Ball.coors.y += Ball.velocity.y;
+    ctx.drawImage(Ball.img, Ball.coors.x, Ball.coors.y, 10, 10);
+  },
+  reset: function() {
+    Ball.coors.x = canvas.width / 2;
+    Ball.coors.y = canvas.height / 2;
+    Ball.velocity.x = 20 * Math.random() - 10;
   }
 }
 
 var Computer = {
   coors: {
-    y: canvas.height / 2 - (plength / 2)
+    x: canvas.width - 30,
+    y: canvas.height / 2 - (PaddleMeta.length / 2)
   },
-  velocity: 1
+  velocity: 0,
+  score: 0,
+  maxVelocity: 0,
+  move: function () {
+    var diff = -(Computer.coors.y - Ball.coors.y);
+    if(diff < 0 && diff < -40) { // max speed left
+      diff = -40;
+    } else if(diff > 0 && diff > 40) { // max speed right
+      diff = 40;
+    }
+
+    ctx.drawImage(PaddleMeta.img, Computer.coors.x, Computer.coors.y - diff, PaddleMeta.height, PaddleMeta.length);
+    if(Computer.coors.y < 0) {
+      Computer.coors.y = 0;
+    } else if (Computer.coors.y + 50 > canvas.height) {
+      Computer.coors.y = canvas.height - Computer.coors.y;
+    }
+  }
 }
 
-var pscore;
-var cscore;
+var Player = {
+  coors: {
+    x: 30,
+    y: canvas.height / 2 - (PaddleMeta.length / 2)
+  },
+  score: 0,
+  velocity: 0,
+  move: function () {
+    if ( Player.coors.y - PaddleMeta.height / 2 <= canvas.height || Player.coors.y + PaddleMeta.height / 2 <= canvas.height) {
+      return;
+    }
+
+    if (findIn("bpaddle") != -1) {
+      var tmpx = Math.abs(Powerups[findIn("bpaddle")]["Ball.coors.x"]);
+      var tmpy = Math.abs(Powerups[findIn("bpaddle")]["Ball.coors.y"]);
+      ctx.fillStyle = "#FF00FF";
+      ctx.fillRect(tmpx - 5, tmpy - 5, 10, 10);
+      if (Math.abs(Powerups[findIn("bpaddle")]["Ball.coors.x"] - Ball.coors.x) < 10 && Math.abs(Powerups[findIn("bpaddle")]["Ball.coors.y"] - Ball.coors.y) < 10) {
+        PaddleMeta.length *= 1.25;
+        Powerups[findIn("bpaddle")]["time"] = Powerups[findIn("bpaddle")]["time"] - 1;
+        if (Powerups[findIn("bpaddle")]["time"] == 0) {
+          PaddleMeta.length = canvas.height / 6;
+          Powerups.splice(Powerups[findIn("bpaddle")], 1);
+        }
+      }
+    }
+    if (findIn("smallpaddle") != -1) {
+      var tmpx = Math.abs(Powerups[findIn("smallpaddle")]["Ball.coors.x"]);
+      var tmpy = Math.abs(Powerups[findIn("smallpaddle")]["Ball.coors.y"]);
+      ctx.fillStyle = "#FFFF00";
+      ctx.fillRect(tmpx - 5, tmpy - 5, 10, 10);
+      if (Math.abs(Powerups[findIn("smallpaddle")]["Ball.coors.x"] - Ball.coors.x) < 10 && Math.abs(Powerups[findIn("smallpaddle")]["Ball.coors.y"] - Ball.coors.y) < 10) {
+        PaddleMeta.length *= 2 / 3;
+
+        Powerups[findIn("smallpaddle")]["time"] = Powerups[findIn("smallpaddle")]["time"] - 1;
+        if (Powerups[findIn("smallpaddle")]["time"] == 0) {
+          PaddleMeta.length = canvas.height / 6;
+          Powerups.splice(Powerups[findIn("smallpaddle")], 1);
+        }
+      }
+    }
+
+
+    ctx.drawImage(PaddleMeta.img, Player.coors.x, Player.coors.y, PaddleMeta.height, PaddleMeta.length);
+  }
+}
 
 var exist = false;
 var run = false;
 var difficulty = 1;
 
 var start = function(e) {
-  e.preventDefault();
-  canvas.width = window.innerWidth - 100;
-  canvas.height = window.innerHeight - 68 - 19 - 40;
-  pscore = 0;
-  cscore = 0;
+  if (e) {
+    e.preventDefault();
+  }
+  PaddleMeta.img.src = "paddle.png";
+  Ball.img.src = "ball.png";
+
   if (!exist) {
     Ball.coors.x = canvas.width - 50;
     Ball.coors.y = canvas.height / 2;
@@ -63,14 +142,41 @@ var start = function(e) {
     exist = !exist;
     run = true;
 
-    window.requestAnimationFrame(play);
+    window.requestAnimationFrame(frame);
   }
 
   if (!run) {
     run = !run;
-    window.requestAnimationFrame(play);
+    window.requestAnimationFrame(frame);
 
   }
+}
+
+var frame = function() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  Ball.move();
+  Player.move();
+  Computer.move();
+
+
+  if (Math.floor(Math.random() * 100) == 1) {
+    randomPowerUp();
+  }
+
+  checkScore();
+  if (run) {
+    window.requestAnimationFrame(frame);
+  }
+}
+
+var findIn = function(a) {
+  for (i = 0; i < Powerups.length; i++) {
+    if (Powerups[i]["type"] == a) {
+      return i;
+    }
+  }
+  return -1;
 }
 
 var clear = function(e) {
@@ -78,180 +184,20 @@ var clear = function(e) {
   exist = false;
   run = false;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  window.cancelAnimationFrame(play);
-}
-
-var play = function() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  movePaddle();
-  moveBall();
-  aiMove();
-  if (Math.floor(Math.random() * 100) == 1) {
-    randomPowerUp();
-  }
-  checkScore();
-  if (run) {
-    window.requestAnimationFrame(play);
-  }
-}
-
-var movePaddle = function() {
-  pycor = pycor + paddlev;
-
-  if (findIn("bpaddle") != -1) {
-    var tmpx = Math.abs(powerups[findIn("bpaddle")]["Ball.coors.x"]);
-    var tmpy = Math.abs(powerups[findIn("bpaddle")]["Ball.coors.y"]);
-    ctx.fillStyle = "#FF00FF";
-    ctx.fillRect(tmpx - 5, tmpy - 5, 10, 10);
-    if (Math.abs(powerups[findIn("bpaddle")]["Ball.coors.x"] - Ball.coors.x) < 10 && Math.abs(powerups[findIn("bpaddle")]["Ball.coors.y"] - Ball.coors.y) < 10) {
-      plength *= 1.25;
-      powerups[findIn("bpaddle")]["time"] = powerups[findIn("bpaddle")]["time"] - 1;
-      if (powerups[findIn("bpaddle")]["time"] == 0) {
-        plength = canvas.height / 6;
-        powerups.splice(powerups[findIn("bpaddle")], 1);
-      }
-    }
-  }
-  if (findIn("smallpaddle") != -1) {
-    var tmpx = Math.abs(powerups[findIn("smallpaddle")]["Ball.coors.x"]);
-    var tmpy = Math.abs(powerups[findIn("smallpaddle")]["Ball.coors.y"]);
-    ctx.fillStyle = "#FFFF00";
-    ctx.fillRect(tmpx - 5, tmpy - 5, 10, 10);
-    if (Math.abs(powerups[findIn("smallpaddle")]["Ball.coors.x"] - Ball.coors.x) < 10 && Math.abs(powerups[findIn("smallpaddle")]["Ball.coors.y"] - Ball.coors.y) < 10) {
-      plength *= 2 / 3;
-
-      powerups[findIn("smallpaddle")]["time"] = powerups[findIn("smallpaddle")]["time"] - 1;
-      if (powerups[findIn("smallpaddle")]["time"] == 0) {
-        plength = canvas.height / 6;
-        powerups.splice(powerups[findIn("smallpaddle")], 1);
-      }
-    }
-  }
-  ctx.drawImage(pad, 30, pycor, pheight, plength);
-}
-
-
-var moveBall = function() {
-  if (Ball.coors.x > canvas.width - 50 && Ball.coors.y > Computer.coors.y && Ball.coors.y < Computer.coors.y + 30) {
-    Ball.velocity.x *= -1
-  } else if (Ball.coors.x < 40 && Ball.coors.y > pycor && Ball.coors.y < pycor + plength) {
-    Ball.velocity.x += paddlev / 2
-    Ball.velocity.x *= -1
-  }
-  if (Ball.coors.y > canvas.height || Ball.coors.y < 0) {
-    Ball.velocity.y *= -1
-  }
-  /*
-  if (findIn("randdir") != -1) {
-      var tmpx = Math.abs(powerups[findIn("randdir")]["Ball.coors.x"]);
-      var tmpy = Math.abs(powerups[findIn("randdir")]["Ball.coors.y"]);
-      ctx.fillStyle = "#FF0000";
-      ctx.fillRect(tmpx - 5, tmpy - 5, 10,10);
-      if (Math.abs(powerups[findIn("randdir")]["Ball.coors.x"] - Ball.coors.x) < 10 && Math.abs(powerups[findIn("randdir")]["Ball.coors.y"] - Ball.coors.y) < 10){
-      Ball.velocity.x = Math.random() * 20 - 10;
-      Ball.velocity.y = Math.random() * 20 - 10;
-      powerups[findIn("randdir")]["time"] = powerups[findIn("randdir")]["time"] - 1;
-      if (powerups[findIn("randdir")]["time"] == 0) {
-        powerups.splice(powerups[findIn("randdir")], 1);
-      }
-      }
-    }
-    if (findIn("slow") != -1) {
-      var tmpx = Math.abs(powerups[findIn("slow")]["Ball.coors.x"]);
-      var tmpy = Math.abs(powerups[findIn("slow")]["Ball.coors.y"]);
-      ctx.fillStyle = "#00FF00";
-      ctx.fillRect(tmpx - 5, tmpy - 5, 10,10);
-      if (Math.abs(powerups[findIn("slow")]["Ball.coors.x"] - Ball.coors.x) < 10 && Math.abs(powerups[findIn("slow")]["Ball.coors.y"] - Ball.coors.y) < 10){
-      Ball.velocity.x = Ball.velocity.x / 2;
-      Ball.velocity.y = Ball.velocity.y / 2;
-      powerups[findIn("slow")]["time"] = powerups[findIn("slow")]["time"] - 1;
-      if (powerups[findIn("slow")]["time"] == 0) {
-        powerups.splice(powerups[findIn("slow")], 1);
-      }
-      }
-    }
-    if (findIn("fast") != -1) {
-      var tmpx = Math.abs(powerups[findIn("fast")]["Ball.coors.x"]);
-      var tmpy = Math.abs(powerups[findIn("fast")]["Ball.coors.y"]);
-      ctx.fillStyle = "#0000FF";
-      ctx.fillRect(tmpx - 5, tmpy - 5, 10,10);
-      if (Math.abs(powerups[findIn("fast")]["Ball.coors.x"] - Ball.coors.x) < 10 && Math.abs(powerups[findIn("fast")]["Ball.coors.y"] - Ball.coors.y) < 10){
-      Ball.velocity.x = Ball.velocity.x * 2;
-      Ball.velocity.y = Ball.velocity.y * 2;
-      powerups[findIn("fast")]["time"] = powerups[findIn("fast")]["time"] - 1;
-      if (powerups[findIn("fast")]["time"] == 0) {
-        powerups.splice(powerups[findIn("fast")], 1);
-      }
-      }
-    }
-  */
-  Ball.coors.x += Ball.velocity.x;
-  Ball.coors.y += Ball.velocity.y;
-  ctx.drawImage(ball, Ball.coors.x, Ball.coors.y, 10, 10);
-}
-
-var findIn = function(a) {
-  for (i = 0; i < powerups.length; i++) {
-    if (powerups[i]["type"] == a) {
-      return i;
-    }
-  }
-  return -1;
+  window.cancelAnimationFrame(frame);
 }
 
 var checkScore = function() {
   if (Ball.coors.x > canvas.width - 30) {
-    pscore = pscore + 1;
-    ballReset()
+    Player.score += 1;
+    Ball.reset()
   }
   if (Ball.coors.x < 30) {
-    cscore = cscore + 1;
-    ballReset()
+    Computer.score += 1;
+    Ball.reset()
   }
-  document.querySelector('.scoreboard .player .score').innerHTML = pscore.toString();
-  document.querySelector('.scoreboard .computer .score').innerHTML = cscore.toString();
-}
-
-var ballReset = function() {
-  Ball.coors.x = canvas.width / 2;
-  Ball.coors.y = canvas.height / 2;
-  Ball.velocity.x = 20 * Math.random() - 10;
-  // Ball.velocity.y = 20 * Math.random() - 10;
-}
-
-/**
- * AI Code
- */
-
-var aiMove = function() {
-  var x = aiPredict();
-}
-
-
-var aiPredict = function() {
-  // var x_pos = ball.x;
-  // var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
-  // if(diff < 0 && diff < -4) { // max speed left
-  //   diff = -5;
-  // } else if(diff > 0 && diff > 4) { // max speed right
-  //   diff = 5;
-  // }
-  // this.paddle.move(diff, 0);
-  // if(this.paddle.x < 0) {
-  //   this.paddle.x = 0;
-  // } else if (this.paddle.x + this.paddle.width > 400) {
-  //   this.paddle.x = 400 - this.paddle.width;
-  // }
-
-  if (Ball.velocity.x > 0) {
-    var t = (canvas.width - 30 - Ball.coors.x) / Ball.velocity.x
-    var x = Math.abs(t * Ball.velocity.y - Ball.coors.y) % (canvas.height - 60) * Math.pow(-1, Math.floor(Math.abs(t * Ball.velocity.y - Ball.coors.y) / (canvas.height - 60)) + (canvas.height - 60) / 2);
-  }
-  if (Ball.velocity.x < 0) {
-    var t = (canvas.width - 90 + Ball.coors.x) / Ball.velocity.x
-    var x = Math.abs(t * Ball.velocity.y - Ball.coors.y) % (canvas.height - 60) * Math.pow(-1, Math.floor(Math.abs(t * Ball.velocity.y - Ball.coors.y) / (canvas.height - 60)) + (canvas.height - 60) / 2);
-  }
-  return x
+  document.querySelector('.scoreboard .player .score').innerHTML = Player.score.toString();
+  document.querySelector('.scoreboard .computer .score').innerHTML = Computer.score.toString();
 }
 
 /**
@@ -280,7 +226,7 @@ var aiPredict = function() {
  var randomPowerUp = function() {
    var types = ["bpaddle", "smallpaddle", "fast", "slow", "randdir"];
    var type = types[Math.floor(Math.random() * 5)];
-   powerups[powerups.length] = makeRandomLoc(type);
+   Powerups[Powerups.length] = makeRandomLoc(type);
  }
 
 /**
@@ -290,29 +236,40 @@ var aiPredict = function() {
 
 document.addEventListener("keyup", function(e) {
   key = e.keyCode;
-  if (key == 40 && paddlev > 0) {
-    paddlev = 0;
+  if (key == 40 && Player.velocity > 0) {
+    Player.velocity = 0;
     e.preventDefault()
-  } else if (key == 38 && paddlev < 0) {
-    paddlev = 0;
+  } else if (key == 38 && Player.velocity < 0) {
+    Player.velocity = 0;
     e.preventDefault()
   }
 });
 
 document.addEventListener("keydown", function(e) {
   key = e.keyCode;
-  if (paddlev == 0) {
-    paddlev = 10;
+  if (Player.velocity == 0) {
+    Player.velocity = canvas.height * 0.05;
   }
 
   if (key == 40) {
-    paddlev = Math.abs(paddlev);
+    Player.velocity = Math.abs(Player.velocity);
     e.preventDefault()
   } else if (key == 38) {
-    paddlev = -1 * Math.abs(paddlev);
+    Player.velocity = -1 * Math.abs(Player.velocity);
     e.preventDefault()
   }
 });
 
-eas.addEventListener("click", start);
-clr.addEventListener("clear", start);
+document.getElementById("clear").addEventListener("click", start);
+document.getElementById("easy").addEventListener("click", function() {
+  Computer.maxVelocity = canvas.height * 0.025;
+  start();
+});
+document.getElementById("medium", function() {
+  Computer.maxVelocity = canvas.height * 0.05;
+  start();
+});
+document.getElementById("hard", function() {
+  Computer.maxVelocity = canvas.height * 0.075;
+  start();
+});
